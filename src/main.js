3852cc6b1702b2656512dec0a273dac65622bf61
wangxy <wangxinyang1983@gmail.com>
2020-07-17 21:52:17 +0900

    config修正

--- src/main.js
+++ src/main.js
@@ -7,10 +7,19 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
+// nprogress
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // Do something before request is sent
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
